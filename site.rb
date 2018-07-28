f44c5aa16bbd3b0f3d99ed59a452eccb0adf8123
Caleb Smith <karagenit@outlook.com>
2018-07-28 09:53:45 -0400

    Request Repo Auth Scope

--- site.rb
+++ site.rb
@@ -8,5 +8,5 @@ CLIENT_ID = File.read('client-id.token')
 CLIENT_SECRET = File.read('client-secret.token')
 
 get '/' do
-  erb :index, locals: { client_id: CLIENT_ID }
+  erb :index, locals: { client_id: CLIENT_ID, scopes: "repo read:user"}
 end
