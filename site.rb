08805f2ea175a3c55da46b96d1e5f6b6e9a8a7a7
Caleb Smith <karagenit@outlook.com>
2018-07-28 09:49:40 -0400

    Add Basic Auth Link

---
+++ site.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'rest-client'
+require 'json'
+
+CLIENT_ID = File.read('client-id.token')
+CLIENT_SECRET = File.read('client-secret.token')
+
+get '/' do
+  erb :index, locals: { client_id: CLIENT_ID }
+end
