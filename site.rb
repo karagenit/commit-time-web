9063c211048b56313389ef3586ffb37e5f3a825b
Caleb Smith <karagenit@outlook.com>
2018-07-28 10:01:38 -0400

    Fetch OAuth Token from Callback

--- site.rb
+++ site.rb
@@ -10,3 +10,19 @@ CLIENT_SECRET = File.read('client-secret.token')
 get '/' do
   erb :index, locals: { client_id: CLIENT_ID, scopes: "repo read:user"}
 end
+
+get '/auth/callback' do
+  github_code = params[:code]
+
+  puts "Code: #{github_code}"
+
+  result = RestClient.post('https://github.com/login/oauth/access_token',
+                          {client_id: CLIENT_ID,
+                           client_secret: CLIENT_SECRET,
+                           code: github_code},
+                           accept: :json)
+
+  token = JSON.parse(result)['access_token']
+
+  puts "Token: #{token}"
+end
