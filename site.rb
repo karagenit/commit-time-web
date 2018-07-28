ca79abdb7d53187b66262bc1f1cb4a2cfe0d831f
Caleb Smith <karagenit@outlook.com>
2018-07-28 10:05:42 -0400

    Move Auth Redirect to Separate Route

--- site.rb
+++ site.rb
@@ -8,7 +8,12 @@ CLIENT_ID = File.read('client-id.token')
 CLIENT_SECRET = File.read('client-secret.token')
 
 get '/' do
-  erb :index, locals: { client_id: CLIENT_ID, scopes: "repo read:user"}
+  erb :index
+end
+
+get '/auth/create' do
+  scopes = "repo read:user"
+  redirect "https://github.com/login/oauth/authorize?scope=#{scopes}&client_id=#{CLIENT_ID}"
 end
 
 get '/auth/callback' do
